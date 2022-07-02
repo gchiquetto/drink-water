b1f45bbc1371ba718f4b378e81568b2807d6b181
Gabriela Chiquetto <gabrielachiquetto@gmail.com>
2022-07-02 11:18:57 -0300

    project finished

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const cups = document.querySelectorAll('.small-cup_container');
 const qtdRemained = document.getElementById('qtd_remained');
 const bigCup = document.querySelector('.big-cup_container');
+const percentage = document.querySelector('.percentage');
 
-const cupFilled = document.createElement('div');
-cupFilled.classList.add('filled');
-bigCup.appendChild(cupFilled);
+// const cupFilled = document.createElement('div');
+// cupFilled.classList.add('filled');
+// bigCup.appendChild(cupFilled);
 document.documentElement.style.setProperty("--filled-row",  '9');
 
 cups.forEach((cup, index) => {
@@ -22,13 +23,17 @@ cups.forEach((cup, index) => {
 });
 
 function updateQtd(cups, index){
- 
+    console.log(index);
     const qtdCups = cups.length;
     const remainedLiters =  2 - qtdCups*0.250;
-    index > 3 ? document.documentElement.style.setProperty("--color",  '#F8FDFF') : document.documentElement.style.setProperty("--color",  '#03256C');
+    const percent = (index + 1)*12.5;
+    console.log(`${percent}%`);
+    index > 6 ?  document.documentElement.style.setProperty("--remained-display",  `none`) : document.documentElement.style.setProperty("--remained-display",  `inline-block`);    
+    //index > 3 ? document.documentElement.style.setProperty("--color",  '#F8FDFF') : document.documentElement.style.setProperty("--color",  '#03256C');
     document.documentElement.style.setProperty("--filled-row",  `${8 - index}`);
-    document.documentElement.style.setProperty("--remained-row",  `${5 - index}`);       
-    index > 7 ?  document.documentElement.style.setProperty("--remained-display",  `none`) : document.documentElement.style.setProperty("--remained-display",  `inline-block`);    
+    document.documentElement.style.setProperty("--remained-row",  `${7 - index}`);       
+    percentage.textContent = `${percent}%`;
     qtdRemained.textContent = `${remainedLiters}L`;
+
 
 }
